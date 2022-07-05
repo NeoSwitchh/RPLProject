cbbda058b8db2c2e1283c3a617709b203a5a8f4f
NeoSwitchh <booklord34@gmail.com>
2022-07-05 08:02:46 +0700

    FIX

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,19 @@
-<html>
-    <head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-</head>
-<body  style="background-image: url('http://127.0.0.1:8000/image/Picture1.png') center;>
+@extends('layouts.app')
+@section('content')
+<body  style="background-image: url('http://127.0.0.1:8000/image/Picture1.png') center;">
     <div class="relative flex items-center min-h-screen" >
         <div class="continer mx-auto flex flex-col lg:flex-row items-center justify-center pb-40 mt-5 pt-5">
             <div class="text-center my-4 lg:my-0 lg:mr-24">
                 <h1 class="text-black text-5xl lg:text-6xl dark:text-white font-bold">VRD Simulation</h1>
                 <h3 class="text-black text-3xl lg:text-4xl dark:text-white font-semibold">a VR-based Disaster Simulation</h3>
-                <div class="mr-5 pt-5">
+                {{-- <div class="mr-5 pt-5">
                     <a href="{{ route('login') }}" class="btn btn-dark ml-5 text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
                     <a href="{{ route('register') }}" class="btn btn-dark ml-5 text-sm text-gray-700 dark:text-gray-500 underline">Register</a
-                </div>
+                </div> --}}
             </div>
             <div class="continer mx-auto flex flex-col lg:flex-row items-center justify-center pb-40">
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> 
     </body> 
-    </html>
+@endsection
