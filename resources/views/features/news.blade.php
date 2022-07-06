55a752065a4e3d6f51b5d2a874b1adad3e4308b4
NeoSwitchh <booklord34@gmail.com>
2022-07-06 08:05:28 +0700

    ADD idk

---
+++ resources/views/features/news.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+        <a href="">Lorem Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorems Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorem Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorems Ipsum</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+        <a href="">Lorem Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorems Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorem Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorems Ipsum</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+        <a href="">Lorem Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorems Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorem Ipsum</a>
+    </div>
+    <div class="col-md-6">
+        <a href="">Lorems Ipsum</a>
+    </div>
+  </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,11 @@
 
     {{-- bootsrap CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;500;600&family=Ubuntu&display=swap"
+        rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -36,6 +41,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/news')}}">News</a>
+                        </li>
                     </ul>
                     
                     <!-- Right Side Of Navbar -->
@@ -81,7 +92,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="font-quicksand dark:bg-slate-600 py-4">
             @yield('content')
         </main>
     </div>
